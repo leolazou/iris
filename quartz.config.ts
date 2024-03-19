5b9bd8de86826125d9ca225acddc42f3f4157fff
leolazou <ileonidasf@gmail.com>
2024-03-20 00:28:55 +0100

    configuring configurations

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,12 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Leo's shared",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "iris.leolazou.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
